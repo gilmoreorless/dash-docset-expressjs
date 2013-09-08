6491a7ac4e4abafe812673247985865b190e8149
Gilmore Davidson <gilmoreorless@gmail.com>
2013-09-08 21:38:47 +1000

    First working version with full docset index

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,7 @@ module.exports = function (grunt) {
   var docsetPathPlist = docsetPath + '/Info.plist';
   var docsetPathDB    = docsetPath + '/Resources/docSet.dsidx';
   var docsetPathDocs  = docsetPath + '/Resources/Documents/';
+  var srcPath = 'src/expressjs.com/';
 
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
@@ -18,10 +19,10 @@ module.exports = function (grunt) {
       },
       docs: {
         files: [
-          {src: 'src/expressjs.com/style.css', dest: docsetPathDocs + 'style.css'},
+          {src: srcPath + 'style.css', dest: docsetPathDocs + 'style.css'},
           {
             expand: true,
-            src: 'src/expressjs.com/images/*',
+            src: srcPath + 'images/*',
             dest: docsetPathDocs + 'images/',
             flatten: true
           }
@@ -29,11 +30,17 @@ module.exports = function (grunt) {
       }
     },
     dom_munger: {
-      docs: {
+      api: {
         options: {
-          remove: 'script, nav.clearfix, #menu'
+          remove: 'script, nav.clearfix, #menu',
+          callback: function ($) {
+            // Convert relative protocol links to explicit http
+            $('link[href^="//"]').attr('href', function (i, oldHref) {
+              return 'http:' + oldHref;
+            });
+          }
         },
-        src: 'src/expressjs.com/api.html',
+        src: srcPath + 'api.html',
         dest: docsetPathDocs + 'api.html'
       }
     }
@@ -45,11 +52,106 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-update-submodules');
   grunt.registerTask('update', ['update_submodules']);
 
-  // TODO: Build docset index
-  // grunt.registerTask('index', function () {});
+  // Build docset index
+  grunt.registerTask('index', function () {
+    var gruntDone = this.async();
+
+    var cheerio = require('cheerio'),
+      Sequelize = require('sequelize'),
+      _ = require('underscore');
+      fs = require('fs');
+
+    var reEndParens = /\(\)$/;
+    var idxGroups;
+
+    // Read the API file and get out the links we want
+    var apiHtml = fs.readFileSync(srcPath + 'api.html');
+    var $ = cheerio.load(apiHtml);
+    idxGroups = $('#menu > li').map(function () {
+      var $li = $(this);
+      // Any top-level links are groups
+      var $link = $li.children('a');
+      var group = {
+        type: 'Guide',
+        name: $link.text(),
+        link: '#' + $link.text().toLowerCase(), // The links point to a method anchor, use the group anchor instead
+        children: []
+      };
+
+      // Find all methods in this group
+      group.children = _.flatten($li.find('ul > li').map(function () {
+        var $link = $(this).children('a');
+        var name = $link.text();
+        var isMethod = reEndParens.test(name);
+        var type = isMethod ? 'Function' : 'Property';
+        if (isMethod) {
+          name = name.replace(reEndParens, '');
+        }
+        return {
+          type: type,
+          name: name,
+          link: $link.attr('href')
+        };
+      }));
+
+      return group;
+    });
+
+    // Create database file
+    // Most of this section copied from https://github.com/exlee/d3-dash-gen
+    var db = new Sequelize('database', 'username', 'password', {
+      dialect: 'sqlite',
+      storage: docsetPathDB
+    });
+
+    // Create the searchIndex table
+    var table = db.define('searchIndex', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      name: {
+        type: Sequelize.TEXT
+      },
+      type: {
+        type: Sequelize.TEXT
+      },
+      path: {
+        type: Sequelize.TEXT
+      }
+    }, {
+      freezeTableName: true,
+      timestamps: false
+    });
+
+    var errorHandler = function () {
+      gruntDone(false);
+    };
+
+    table.sync({force: true}).success(function () {
+      // Add the data
+      var buildRowData = function (data) {
+        return {
+          name: data.name,
+          type: data.type,
+          path: 'api.html' + data.link
+        };
+      };
+
+      var rows = _.flatten(_.map(idxGroups, function (group) {
+        return [group].concat(group.children);
+      }));
+
+      table.bulkCreate(_.map(rows, buildRowData))
+        .success(gruntDone)
+        .error(errorHandler);
+
+    }).error(errorHandler);
+  });
 
   // Main task
-  grunt.registerTask('generate', ['update', 'copy:meta', 'copy:docs', 'dom_munger:docs']);
+  grunt.registerTask('generate', ['update', 'copy:meta', 'copy:docs', 'dom_munger:api', 'index']);
   grunt.registerTask('default', ['generate']);
 
 };
